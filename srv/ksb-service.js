2f6a7e859d09c6c43a34ef741f81f6db123a6fae
Martin Schlindwein <martin.schlindwein@ksb.com>
2024-11-13 16:59:23 +0000

    Martin

--- srv/ksb-service.js
+++ srv/ksb-service.js
@@ -1,3 +1,5 @@
+const { log } = require('@sap/cds');
+
 module.exports = function () {
 
   const {
@@ -13,11 +15,14 @@ module.exports = function () {
     } = await import('@sap-ai-sdk/foundation-models');
 
     const tender_instruction = `
-      Extract from the text at the end of this prompt if 
+    You are a valid json interpreter.  
+    Extract from the text at the end of this prompt if 
       a pump is requested in the text (haspump) and if there any hints that the 
       pump should be manufactured by a specific company other than KSB (hascompetitor).
       Provide the response as JSON in the example format below.
+      The answer must be a valid json in the valid json format, nothing else.
       There should be only one JSON message for the complete text.
+      Please remove any unnecessary characters.
       
       {
              "HasPump": "True",
@@ -25,7 +30,7 @@ module.exports = function () {
       }
       `;
 
-    const tender_llmRequest = tender_instruction + query[0].TenderContent
+    let tender_llmRequest = tender_instruction + query[0].TenderContent
     //console.log('LLM request: ', llmRequest)
 
 
@@ -42,7 +47,10 @@ module.exports = function () {
       ]
     });
 
-    const tender_responseContent = response.getContent();
+    let tender_responseContent = response.getContent();
+    tender_responseContent = tender_responseContent.replace('```json','');
+    tender_responseContent = tender_responseContent.replace('```','');
+
     console.log(tender_responseContent)
 
     const requirements_instruction = `
@@ -51,7 +59,7 @@ module.exports = function () {
       the response as JSON in the example format below.
       Return only the number values for above JSON without the units.
       For every detected pump there should be one JSON message.
-      The answer must be a valid json in the valid json format, nothing else.
+      The answer must be a valid json array in the valid json format, nothing else.
       Please just return plain text without any JSON formatting.
       
       {
@@ -77,14 +85,16 @@ module.exports = function () {
       ]
     });
 
-    const requirements_responseContent = response.getContent();
+    let requirements_responseContent = response.getContent();
+    requirements_responseContent = requirements_responseContent.replace('```json','');
+    requirements_responseContent = requirements_responseContent.replace('```','');
     console.log(requirements_responseContent)
 
     //String der Tender-Response als json
     let jsonObject = JSON.parse(tender_responseContent);
-      console.log("Resulting json: \n", jsonObject)
+    console.log("Resulting json: \n", jsonObject)
 
-      //Speichern des Tenders ANALYZE mit:
+    //Speichern des Tenders ANALYZE mit:
     // - Last analyzed at: <timestamp>
     // - Description
     // - Tender Content
@@ -93,40 +103,91 @@ module.exports = function () {
     //...
 
     const lastAnalyzedAt = new Date();
-    const tender = { Description: query[0].Description, TenderContent: query[0].TenderContent, LastAnalyzedAt: lastAnalyzedAt, ContainsPump: jsonObject.ContainsPump, HasCompetitor: jsonObject.HasCompetitor };  
+    const tender = { Description: query[0].Description, TenderContent: query[0].TenderContent, LastAnalyzedAt: lastAnalyzedAt, ContainsPump: jsonObject.ContainsPump, HasCompetitor: jsonObject.HasCompetitor };
     //await INSERT.into(Tender).entries(tender);
     //await cds.tx(req).run(INSERT.into(Tender).entries(tender));
     let tenderID = query[0].ID;
-    await UPDATE(Tender).where({ID: tenderID}).set(tender);
+    await UPDATE(Tender).where({ ID: tenderID }).set(tender);
 
     //String der Requirements-Responses jeweils als json
     //@Loop über die json Requirements!
-    jsonObject = JSON.parse(requirement_responseContent);
+    jsonObject = JSON.parse(requirements_responseContent);
 
     console.log("Concat HZ to SupplyFrequency")
-      jsonObject.SupplyFrequency += 'HZ'
- 
-      console.log("Concat V to RatedVoltage")
-      jsonObject.RatedVoltage += 'V'
- 
-      console.log("Resulting json: \n", jsonObject)
-  
+    jsonObject.SupplyFrequency += 'HZ'
+
+    console.log("Concat V to RatedVoltage")
+    jsonObject.RatedVoltage += 'V'
+
+    console.log("Resulting json: \n", jsonObject)
+
     //@todo: Requirements speichern:
     // - Flow, Head, Voltage mit V ohne Leerzeichen, Frequenz mit Hz ohne Leerzeichen
 
     const prd_rcmd = await cds.connect.to('API_PRODUCT_RECOMMENDATION');
     const prd_rcmd_response = await prd_rcmd.send({
       method: "POST",
-      path: "/Bulk",
+      path: "/DetermineProductSrv/Root",
       data: {
-        
+        "mlModel": "MDL62BB0FC9-FCE1-472A-9F67-1A76B10F977C",
+        "productCategory": "Pumps",
+        "topN": 1,
+        "features": [
+          {
+            "name": "KSB_FLOW",
+            "value": "30"
+          },
+          {
+            "name": "KSB_HEAD",
+            "value": "50"
+          },
+          {
+            "name": "KSB_SUPPLYFREQUENCY",
+            "value": "50HZ"
+          },
+          {
+            "name": "KSB_RATEDVOLTAGE",
+            "value": "400V"
+          }
+        ],
+        "lowerthreshold": 0.00
       }
-
     });
 
+    const prd_rcmd_response_2 = await prd_rcmd.send({
+      method: "POST",
+      path: "/DetermineProductConfigurationSrv/Root",
+      data: {
+        "topN": 1,
+        "features": [
+          {
+            "name": "KSB_FLOW",
+            "value": "30"
+          },
+          {
+            "name": "KSB_HEAD",
+            "value": "50"
+          },
+          {
+            "name": "KSB_SUPPLYFREQUENCY",
+            "value": "50HZ"
+          },
+          {
+            "name": "KSB_RATEDVOLTAGE",
+            "value": "400V"
+          },
+          {
+            "name": "KMAT",
+            "value": "S01B0T"
+          }
+        ],
+        "commercialAttributesDeterminationModel": "MDLD73A2552-8AEC-4E47-8071-F5A4699E61C1",
+        "configurationDeterminationModel": "MDL71D1A5B4-8848-470C-9498-9971829954EF"
+      }
+    });
 
-
-
-
+    console.log(prd_rcmd_response);
+    console.log('_________________________________')
+    console - log(prd_rcmd_response_2);
   })
 }
